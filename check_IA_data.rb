730d72fc46f59cd10ad4b96702d82dd732053ab1
ldss-jm <ldss-jm@users.noreply.github.com>
2018-02-05 14:57:20 -0500

    make check_IA_data handle recent IARecord and SierraBib updates

--- check_IA_data.rb
+++ check_IA_data.rb
@@ -1,6 +1,6 @@
-load 'IASierraBib.rb'
-load 'IASierra856.rb'
-load 'IARecord.rb'
+require_relative 'IASierraBib'
+require_relative 'IASierra856'
+require_relative 'IARecord'
 
 
 ifile = IARecord.import_search_csv('search.csv')
@@ -42,11 +42,8 @@ bnums.entries.each do |temp_bnum, ia_recs|
   ia_recs.each do |ia|
     puts ia.id
     notes = []
-    if bib.record_id
-      notes << 'bib suppressed' if bib.suppressed
-    else
-      notes << 'bib deleted' 
-    end
+    notes << 'bib deleted' if bib.deleted
+    notes << 'bib suppressed' if bib.suppressed
     unless bib.ia.select { |ia| ia.lacks_caption? }.empty?
       # if bib has any ia with vol info that lacks caption
       # may as well warn about any on bib, such that with "v.2" and "2"
@@ -58,25 +55,33 @@ bnums.entries.each do |temp_bnum, ia_recs|
         notes << 'other ia_rec on bib lacks a caption'
       end
     end
+    #if ia has no vol info but other recs on IA bib
+    if !ia.volume && bib.ia.length > 1
+      notes << 'this ia.vol empty but multiple recs on bib'
+    end
     #if bib.has multiple recs with no vol info
     if ia_count_by_vol[''] && ia_count_by_vol[''] > 1
       notes << 'bib has multiple recs with no vol info'
     end
-    #if bib had 1 rec with no vol info and other recs with info
+    #if bib has 1 rec with no vol info and other recs with info
     if ia_count_by_vol.length > 1 and ia_count_by_vol['']
       notes << 'bib has rec(s) that lack vol info and others that do not'
     end
-    #if bib is serial and ia has no vol info
-    # bib.record_id check skips this for deleted bibs
-    if bib.record_id && bib.serial? && ia.volume.empty?
+    #if ia has no vol info and bib is a serial
+    if !bib.deleted && !ia.volume && bib.serial? 
       notes << 'this ia.vol empty but bib is a serial'
       #warn true
     end
     #if ia has other rec in ia with same vol info
-    if !ia.volume.empty? && ia_count_by_vol[ia.volume] > 1
+    if ia.volume && ia_count_by_vol[ia.volume] > 1
       notes << 'this ia.vol equals other ia.vol on same bib, dupe?'
     end
-    htstatus = arks.include?(ia.ark) ? 'ark_in_HT' : 'ark_not_in_HT'
+    htstatus =
+      if arks.include?(ia.ark)
+        'ark_in_HT'
+      else
+        'ark_not_in_HT'
+      end
     ofile << [
       ia.id,
       notes.join(';;;'),
