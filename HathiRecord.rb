83db500847ca46876f26e530643e3517272f870b
ldss-jm <ldss-jm@users.noreply.github.com>
2018-03-08 15:07:27 -0500

    count fields better; only do $300 checks on monos

--- HathiRecord.rb
+++ HathiRecord.rb
@@ -124,23 +124,23 @@ class HathiRecord
     end
 
     # check 001
-    if @marc.no_001?
+    if @marc.count('001') == 0
       warn('This bib does not contain an 001 field, which it should have. Report to cataloging staff to fix 001 field.')
     end
-    if @marc.multiple_001?
+    if @marc.count('001') >= 2
       warn('This bib has more than one 001 field, which is a non-repeatable field. Report to cataloging staff to fix 001 field.')
     end
 
      # check 003
-    if @marc.multiple_003?
+    if @marc.count('003') >= 2
       warn('This bib has more than one 003 field, which is a non-repeatable field. Report to cataloging staff to fix 003 field.')
     end
 
     # check 008
-    if @marc.no_008?
+    if @marc.count('008') == 0
       warn('This bib does not contain an 008 field, which is a required field. Report to cataloging staff to fix 008 field.')
     end
-    if @marc.multiple_008?
+    if @marc.count('008') >= 2
       warn('This bib has more than one 008 field, which is a non-repeatable field. Report to cataloging staff to fix 008 field.')
     end    
     if @marc.bad_008_length?
@@ -152,10 +152,10 @@ class HathiRecord
     end
 
     # check 245
-    if @marc.no_245?
+    if @marc.count('245') == 0
       warn('This bib does not contain an 245 field, which is a required field. Report to cataloging staff to fix 245 field.')
     end
-    if @marc.multiple_245?
+    if @marc.count('245') >= 2
       warn('This bib has more than one 245 field, which is a non-repeatable field. Report to cataloging staff to fix 245 field.')
     end
     if @marc.no_245_has_ak?
@@ -163,20 +163,20 @@ class HathiRecord
     end
 
     # check 300
-    if @marc.no_300?
-      warn('This bib does not contain an 300 field, which we need for HathiTrust. Report to cataloging staff to fix 300 field.')
+    if @marc.count('300') == 0 && @sierra.blvl !~ /b|i|s/
+      warn('This mono bib does not contain an 300 field, which we need for HathiTrust. Report to cataloging staff to fix 300 field.')
     end
-    if @marc.m300_without_a?
-      warn('This bib contains a 300 without a 300$a, which should not be for HathiTrust. Report to cataloging staff to fix 300 field.')
+    if @marc.m300_without_a? && @sierra.blvl !~ /b|i|s/
+      warn('This mono bib contains a 300 without a 300$a, which should not be for HathiTrust. Report to cataloging staff to fix 300 field.')
     end
 
     # check oclc 035s
     # this needs to be @hathi_marc rather than @marc b/c @marc does not contain
     # 035s we've written from an 001
-    if @hathi_marc.no_oclc_035?
+    if @hathi_marc.oclc_035_count == 0
       warn('This bib does not contain an 035 field with an OCLC number and does not have an OCLC number in the 001. An OCLC number is required for Hathitrust. Report to cataloging staff.')
     end
-    if @hathi_marc.multiple_oclc_035?
+    if @hathi_marc.oclc_035_count >= 2
       warn('This bib contains multiple 035 fields with OCLC numbers or contains an 035 and an 001 with distinct OCLC numbers. Report to cataloging staff to fix.')
     end
   end
